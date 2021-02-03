21ea6a24807a78b8f79732e2e4c8b0a73450225a
Zaied Yousuf <zaiedyousuf3@gmail.com>
2021-02-04 02:56:32 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,19 @@
+//let a;
+//function add(num1,num2){
+//    console.log(num1+num2);
+//    return
+//}
+
+//function add(num1,num2){
+//    console.log(num1,num2);
+//}
+
+//const person = {name:'smart dude',phone:"010101"};
+//const {address} = person;
+//console.log(address);
+
+//let fun = undefined;
+//console.log(fun);
+
+const arr = [5,9,1];
+console.log(arr[13]);
